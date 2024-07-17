08fbdf19508caabf37c7dab5a2abebbe32922bcb
SOUVIK KUMAR DEY <117576556+souvik-dey-sniC@users.noreply.github.com>
2024-07-17 19:19:39 +0000

    datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Souvik"; //String
+let age = 22; //Number
+let isLoggedIn = false; //Boolean
+let state = null;
+
+console.table([name, age, isLoggedIn, state]);
+//Premitive Datatypes
+//Number => 2 to power 53
+//Bigint
+//String => ""
+//Boolean => True/False
+//Null => Standlone Value
+//Undefined
+//Symbol => Unique
+
+//Object
+
+console.log(typeof "Souvik");
+console.log(typeof null); //Object
+console.log(typeof undefined); //Undefined
