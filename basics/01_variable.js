2b596ebdf16c17a4b77c0ed4bb21e548075136da
SOUVIK KUMAR DEY <117576556+souvik-dey-sniC@users.noreply.github.com>
2024-07-17 19:09:10 +0000

    conyainer config done

---
+++ basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "souvik23@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let accontState;
+
+//accountId = 2;
+accountEmail = "hc@hc.com";
+accountPassword = "1234";
+accountCity = "Newtown";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accontState]);
