87418fefc3f2eafcdf64ff2cf790e7d674bc6ea8
SOUVIK KUMAR DEY <117576556+souvik-dey-sniC@users.noreply.github.com>
2024-07-19 18:47:29 +0000

    number & maths in js

---
+++ basics/06_number_and_maths.js
@@ -0,0 +1,38 @@
+// const score = 500;
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(balance.toExponential());
+
+// console.log(Number.EPSILON);
+// console.log(Number.isFinite());
+// console.log(Number.isInteger());
+// console.log(Number.MAX_VALUE);
+
+//*********** MATHS ********** */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 6, 9, 4, 7.9, 3.8));
+// console.log(Math.max(4, 6, 9, 4, 7.9, 3.8));
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
