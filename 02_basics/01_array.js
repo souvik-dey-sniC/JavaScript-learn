454501a2886846993e196c4c1947a14456ad0088
SOUVIK KUMAR DEY <117576556+souvik-dey-sniC@users.noreply.github.com>
2024-07-21 16:21:46 +0000

    array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,35 @@
+// const myArr = [0, 1, 2, 3, 4];
+// const myHeros = ["Saktimaan", "Iron Man", "Captain America"];
+// const myArr2 = new Array("Souvik", "Dey");
+
+// console.log(myArr[5]);
+// console.log(myHeros);
+// console.log(myArr2);
+
+//Araay Methods
+const myArr = [0, 1, 2, 3, 4];
+// console.log(myArr);
+// console.log(typeof(myArr));
+// myArr.push(5);
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// myArr.unshift(8);
+// myArr.shift();
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(5));
+// console.log(myArr);
+
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+//Slice, Splice
+console.log("A ", myArr);
+const mynA1 = myArr.slice(1, 3);
+console.log(mynA1);
+console.log("B ", myArr);
+
+const mynA2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(mynA2);
